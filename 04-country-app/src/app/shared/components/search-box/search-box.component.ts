70c68fb6fee7ca4e227a3a3418e1429e956441de
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2023-06-18 20:13:33 -0600

    country app completed - Section 9

---
+++ 04-country-app/src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: [],
+})
+export class SearchBoxComponent {
+  @Input()
+  public placeholder: string = '';
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter<string>();
+
+  onValueChange(value: string) {
+    this.onValue.emit(value);
+  }
+}
